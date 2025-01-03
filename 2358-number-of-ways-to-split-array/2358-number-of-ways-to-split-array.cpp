74af15a51f97b265340850928f84fe8b82a23d7e
Kanikawadhwa28 <kanika.28.w@gmail.com>
2025-01-03 20:00:57 +0530

    Time: 0 ms (100%), Space: 89.3 MB (62.32%) - LeetHub

--- 2358-number-of-ways-to-split-array/2358-number-of-ways-to-split-array.cpp
+++ 2358-number-of-ways-to-split-array/2358-number-of-ways-to-split-array.cpp
@@ -1,28 +1,41 @@
 class Solution {
 public:
     int waysToSplitArray(vector<int>& nums) {
-        int n = nums.size();
-        int min_splits = n-1;
-        vector<long long> suffixsum(n,0);
-        vector<long long> prefixsum(n,0);
-        suffixsum[n-1]= nums[n-1];
-        for(int i=n-2;i>=0;i--){
-            suffixsum[i]=nums[i]+suffixsum[i+1];
+        long long leftsum=0, rightsum=0;
+        for(auto i :nums){
+            rightsum+= i;
         }
-        // for(auto i :suffixsum){
-        //     cout<<i<<" ";
-        // }cout<<endl;
-        prefixsum[0]=nums[0];
-        for(int i=1;i<n;i++){
-            prefixsum[i]=nums[i]+prefixsum[i-1];
-        }
-        //   for(auto i :prefixsum){
-        //     cout<<i<<" ";
-        // }cout<<endl;
-        int count =0;
-        for(int i=0;i<n-1;i++){
-            if(prefixsum[i]>=suffixsum[i+1]) count++;
+        int count=0;
+        for(int i=0;i<nums.size()-1;i++){
+            leftsum+= nums[i];
+            rightsum-=nums[i];
+            if(leftsum >= rightsum)
+                count++;
         }
         return count;
     }
+        // int n = nums.size();
+        // int min_splits = n-1;
+        // vector<long long> suffixsum(n,0);
+        // vector<long long> prefixsum(n,0);
+        // suffixsum[n-1]= nums[n-1];
+        // for(int i=n-2;i>=0;i--){
+        //     suffixsum[i]=nums[i]+suffixsum[i+1];
+        // }
+        // // for(auto i :suffixsum){
+        // //     cout<<i<<" ";
+        // // }cout<<endl;
+        // prefixsum[0]=nums[0];
+        // for(int i=1;i<n;i++){
+        //     prefixsum[i]=nums[i]+prefixsum[i-1];
+        // }
+        // //   for(auto i :prefixsum){
+        // //     cout<<i<<" ";
+        // // }cout<<endl;
+        // int count =0;
+        // for(int i=0;i<n-1;i++){
+        //     if(prefixsum[i]>=suffixsum[i+1]) count++;
+        // }
+        // return count;
+    // }
 };
