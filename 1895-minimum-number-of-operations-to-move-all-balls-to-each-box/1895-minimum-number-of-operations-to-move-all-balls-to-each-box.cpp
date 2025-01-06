d5f094c1d7eda3999f1f9ec759a66a93abb30a23
Kanikawadhwa28 <kanika.28.w@gmail.com>
2025-01-06 23:48:04 +0530

    Time: 4 ms (57.5%), Space: 11.9 MB (80.39%) - LeetHub

--- 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/1895-minimum-number-of-operations-to-move-all-balls-to-each-box.cpp
+++ 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/1895-minimum-number-of-operations-to-move-all-balls-to-each-box.cpp
@@ -1,24 +1,36 @@
 class Solution {
 public:
     vector<int> minOperations(string boxes) {
-        vector<int> ans;
-        // int counto=0;
-        // for(auto i :boxes){
-        //     if(i=='1') {
-        //         counto++;
-        //         pos.push_back(i);
-        //     }
-        // }
-        int n = boxes.size();
-        for(int i=0;i<n;i++){
-            int count=0;
-            for(int j=0;j<n;j++){
-                if(boxes[j]=='1'){
-                    count+= abs(j-i);
-                }
+        int leftsum=0,rightsum=0,prefix_sum=0,sum=0;
+        for(int i=0;i<boxes.size();i++){
+            if(boxes[i]=='1'){
+                rightsum++;
+                sum+=i;
             }
-            ans.push_back(count);
         }
-        return ans;
+        vector<int> res(boxes.size());
+        for(int i=0;i<boxes.size();i++){
+            int moves = prefix_sum+sum;
+            res[i]=moves;
+            if(boxes[i]=='1'){
+                rightsum--;
+                leftsum++;
+            }
+            sum-=rightsum;
+            prefix_sum+=leftsum;
+        }
+        return res;
+        // vector<int> ans;
+        // int n = boxes.size();
+        // for(int i=0;i<n;i++){
+        //     int count=0;
+        //     for(int j=0;j<n;j++){
+        //         if(boxes[j]=='1'){
+        //             count+= abs(j-i);
+        //         }
+        //     }
+        //     ans.push_back(count);
+        // }
+        // return ans;
     }
 };
