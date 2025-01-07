39665d67507b6ab4150bc17f59e1774fdcd919f9
Kanikawadhwa28 <kanika.28.w@gmail.com>
2025-01-07 18:36:41 +0530

    Time: 0 ms (100%), Space: 11.4 MB (60.52%) - LeetHub

--- 1524-string-matching-in-an-array/1524-string-matching-in-an-array.cpp
+++ 1524-string-matching-in-an-array/1524-string-matching-in-an-array.cpp
@@ -2,16 +2,17 @@ class Solution {
 public:
     vector<string> stringMatching(vector<string>& words) {
         int n = words.size();
-        set<string> ans;
+        vector<string> ans;
         for(int i=0;i<n;i++){
             for(int j=0;j<n;j++){
                 if(i!=j && words[j].find(words[i]) != string::npos){
-                    ans.insert(words[i]);
-                   // ans.push_back(words[i]);
+                    // ans.insert(words[i]);
+                   ans.push_back(words[i]);
+                  break;
                 }
             }
         }
-       vector<string> result(ans.begin(),ans.end());
-        return result;
+    //    vector<string> result(ans.begin(),ans.end());
+        return ans;
     }
 };
